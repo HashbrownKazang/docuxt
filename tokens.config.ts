7203506aa5315ec4c784f435b1f55be50292a957
HashbrownKazang <admin@spuntentertainment.com>
2023-09-22 21:17:30 -0400

    edit on studio

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,27 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  docus: {
+    header: {
+      height: '72px',
+
+      logo: {
+        height: {
+          initial: '1.75rem',
+          sm: '2rem'
+        }
+      },
+
+      title: {
+        color: {
+          static: {
+            initial: '#ff99ff',
+            dark: '#ff99ff'
+          },
+
+          hover: '#ff99ff'
+        }
+      }
+    }
+  }
 })
